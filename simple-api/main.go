a845d17be8c4f430dd7fb5d04b065e0495a18f40
Benjamin Shyong <ben.shyong@gmail.com>
2015-12-09 20:42:58 -0800

    weather mock

--- simple-api/main.go
+++ simple-api/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"strings"
 )
 
 // middleware
@@ -25,8 +27,35 @@ func final(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("ok"))
 }
 
+func query(city string) (weatherData, error) {
+	resp, err := http.Get("http://api.openweathermap.org/data/2.5/weather?APPID=9632c7e59d791cd231f4e2a79310d139&q=" + city)
+	if err != nil {
+		return weatherData{}, nil
+	}
+
+	defer resp.Body.Close()
+
+	var d weatherData
+
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, nil
+	}
+	return d, nil
+}
+
 func main() {
 	finalHander := http.HandlerFunc(final)
+
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		city := strings.SplitN(r.URL.Path, "/", 3)[2]
+		data, err := query(city)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json; charset-utf-8")
+		json.NewEncoder(w).Encode(data)
+	})
 
 	http.Handle("/", middlewareOne(middlewareTwo(finalHander)))
 	http.ListenAndServe(":8080", nil)
---
+++ simple-api/structs.go
@@ -0,0 +1,9 @@
+package main
+
+// name, type, tag
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
